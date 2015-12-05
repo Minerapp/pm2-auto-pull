1ad23a1b55f0ef864b3686c99c608364c49249b5
Michael Hernandez <michael.hernandez1988@gmail.com>
2015-12-05 09:48:44 -0500

    Adding ability to install and uninstall packages based on difference in package.json

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 
 process.env.MODULE_DEBUG = (process.NODE_ENV == 'production' ? false : true);
 
+var fs      = require('fs');
+var path    = require('path');
 var pmx     = require('pmx');
 var pm2     = require('pm2');
 var async   = require('async');
 var pkg     = require('./package.json');
 var debug   = require('debug')(pkg.name);
+var depDiff = require('dependency-diff');
+var spawn   = require('child_process').spawn;
 
 var conf    = pmx.initModule();
+var Probe = pmx.probe();
 
+var app_updated = Probe.counter({
+  name : 'App updated'
+});
 function autoPull(cb) {
   pm2.list(function(err, procs) {
 
@@ -16,22 +24,32 @@ function autoPull(cb) {
       if (proc.pm2_env && proc.pm2_env.versioning) {
         console.log(proc.pm2_env.pm_cwd);
         debug('pull And Reload %s', proc.name);
+        var before = JSON.parse(fs.readFileSync(path.join(proc.pm2_env.pm_cwd, 'package.json'), 'utf8'));
         pm2.pullAndReload(proc.name, function(err, meta) {
           if (meta) {
             var rev = meta.rev;
-
+            var after = JSON.parse(fs.readFileSync(path.join(proc.pm2_env.pm_cwd, 'package.json'), 'utf8'));
             app_updated.inc();
-
-            if (rev)
-              console.log('Successfully pulled [App name: %s] [Commit id: %s] [Repo: %s] [Branch: %s]',
-                          proc.name,
-                          rev.current_revision,
-                          meta.procs[0].pm2_env.versioning.repo_path,
-                          meta.procs[0].pm2_env.versioning.branch);
-            else {
-              // Backward compatibility
-              console.log('App %s succesfully pulled');
-            }
+            var cmdList = depDiff().left(before).right(after).toCmdList();
+            console.log('executing', cmdList)
+            
+            pm2.stop(proc.name, function (err, meta) {
+              async.map(cmdList, installDependencies, function(err, res) {
+                console.log(err,res);
+                pm2.restart(proc.name, function (err, meta) {
+                if (rev)
+                  console.log('Successfully pulled [App name: %s] [Commit id: %s] [Repo: %s] [Branch: %s]',
+                              proc.name,
+                              rev.current_revision,
+                              meta.procs[0].pm2_env.versioning.repo_path,
+                              meta.procs[0].pm2_env.versioning.branch);
+                else {
+                  // Backward compatibility
+                  console.log('App %s succesfully pulled');
+                }
+                })
+              })
+            })
           }
           if (err)
             debug('App %s already at latest version', proc.name);
@@ -42,6 +60,22 @@ function autoPull(cb) {
     }, cb);
 
   });
+}
+
+function installDependencies(cmd, cb) {
+  cmd = cmd.split(' ');
+  cmd[2] = cmd[2].replace(/"/g, '');
+  var child = spawn(cmd.splice(0,1)[0], cmd)
+  child.stdout.pipe(process.stdout)
+  child.stderr.pipe(process.stderr)
+  child.on('error', cb);
+  child.on('close', function (code) {
+    if (code === 0) {
+      return cb(null, true);
+    } else {
+      return cb(new Error('child process exited with non-zero exit code'), code);
+    }
+  })
 }
 
 pm2.connect(function() {
@@ -56,7 +90,7 @@ pm2.connect(function() {
     autoPull(function() {
       running = false;
     });
-  }, 30000 || conf.interval);
+  }, 1000 || conf.interval);
 
 });
 
